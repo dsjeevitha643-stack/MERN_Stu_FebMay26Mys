f05dc4ee3cb3b5d0cd23a63055af6e5a53f3ad80
dsjeevitha643-stack <dsjeevitha643@gmail.com>
2026-03-03 15:44:26 +0530

    Arithmetic Operators

--- W3/D2/P1.js
+++ W3/D2/P1.js
@@ -6,7 +6,7 @@ console.log("a+b: ",a+b);
 console.log("a-b: ",a-b);
 console.log("a*b: ",a*b);
 console.log("a/b: ",a/b);
-console.log("a%b: ",a%b);  //Modulus
+console.log("a%b: ",a%b);  //Modulu
 console.log("a**b: ",a**b); //Exponential
 console.log("a++:",a++);  //post-increment
 console.log("++a:",++a);  //pre-increment
