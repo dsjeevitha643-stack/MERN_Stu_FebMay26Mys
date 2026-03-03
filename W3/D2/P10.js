eaeccf211626fa3371a15e6f292cc879115bd5fc
dsjeevitha643-stack <dsjeevitha643@gmail.com>
2026-03-03 14:55:36 +0530

    message

---
+++ W3/D2/P10.js
@@ -0,0 +1,10 @@
+//Callback function
+//Is a function which is passed an argument to another function
+
+function greetUser(name, Callback){
+    console.log("Hello, ",name);
+    Callback();
+}
+greetUser("Jeevitha",function(){
+    console.log("Callback function executed");
+});
