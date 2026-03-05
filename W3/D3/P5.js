c4d5318cbe9269c4cad674329e17d3db9db7c1ca
dsjeevitha643-stack <dsjeevitha643@gmail.com>
2026-03-05 13:46:22 +0530

    Basics of Objects

--- W3/D3/P5.js
+++ W3/D3/P5.js
@@ -1,5 +1,5 @@
-// //  basics of Objects
- const person ={name:'khushi', age:'22', isStudent: false};
+// Basics of Objects
+const person ={name:'khushi', age:'22', isStudent: false};
 console.log("person", person);
 console.log("Name",person.name);
 console.log("Age:", person['age']);
