88a3fb799e088ce81f278ab0867c9cc349f6ce24
dsjeevitha643-stack <dsjeevitha643@gmail.com>
2026-03-07 22:52:29 +0530

    Filter method

---
+++ W3/D4/P5.js
@@ -0,0 +1,11 @@
+// Reduce method
+let num = [5,10,15];
+let average = num.reduce((intermediateSum,current) => intermediateSum+current,0) / num.length;
+// console.log(average);
+
+//Reduce to object count by category
+let items = ["pen","pencile","eraser","pen"];
+let count = items.reduce((intermediateValue,item )=>{intermediateValue[item] = (intermediateValue[item] || 0)+1;
+    return intermediateValue;
+},{});
+console.log("Item count: ",count);
