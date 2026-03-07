7aba06a811d141c90e3c1e74009101a0d18884e9
dsjeevitha643-stack <dsjeevitha643@gmail.com>
2026-03-07 22:51:23 +0530

    Filter method

--- W3/D4/P4.js
+++ W3/D4/P4.js
@@ -1,1 +1,17 @@
-// Filter
+// Filter method
+// let marks = [75,49,56,70,82,51,68];
+// let passed = marks.filter(mark => mark >= 70);
+// console.log(marks);
+// console.log(passed);
+
+let student = [
+    {name:"Arjun", score: 70},
+    {name: "Krishna",score:82},
+    {name:"Raj",score:56},
+    {name:"Radha",score:68},
+];
+let qulifiedStudent = student.filter(s => s.score < 70 );
+console.log(qulifiedStudent);
+
+
+
