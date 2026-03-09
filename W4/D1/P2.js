54809688db0d4409da065f20035c172149931f86
dsjeevitha643-stack <dsjeevitha643@gmail.com>
2026-03-09 17:38:53 +0530

    try catch and finally block

---
+++ W4/D1/P2.js
@@ -0,0 +1,18 @@
+// try catch and finally block
+function parseConfig(text){
+    try{
+        let config = JSON.parse(text);
+        console.log(config);
+        return config;
+    }
+    catch(err){
+        console.log("Error parsing config: ",err.message);
+        return null;
+    }
+    finally{
+        console.log("Finally block executed.");
+    }
+
+}
+parseConfig('{"theme":"dark"}');
+parseConfig("json");
