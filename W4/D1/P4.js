df8af4799cf80f6af5dbc91aad82279b700d1b77
dsjeevitha643-stack <dsjeevitha643@gmail.com>
2026-03-09 17:39:49 +0530

    Logging

---
+++ W4/D1/P4.js
@@ -0,0 +1,24 @@
+// Logging 
+console.log("console logging");
+console.warn("Warning message");
+console.error("Error message");
+let users = [
+    {id:1,name:"Amir"},
+    {id:2,name:"Amrutha"},
+];
+console.log(users);
+console.table(users);
+
+//Group related logs
+console.group("Grouped logs");
+console.log("Log 1");
+console.log("Log 2");
+console.log("Log 3");
+console.groupEnd();
+
+// measure execution time
+console.time("LoopTimer");
+for(let i = 0; i<1000 ;i++){
+
+}
+console.timeEnd("LoopTimer");
