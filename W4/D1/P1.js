1885462f746afc45287f576ae1e71707cbc1d007
dsjeevitha643-stack <dsjeevitha643@gmail.com>
2026-03-09 17:38:08 +0530

    Error handling

---
+++ W4/D1/P1.js
@@ -0,0 +1,26 @@
+try{
+    console.log("Trying to access undefined variable");
+    console.log(notDefined);
+}
+catch(err){
+    console.log("Error caught",err.name,"-",err.message);
+}
+console.log("Program execution continues");
+
+//JSON Parsing error
+let jsonText = "{json}";
+try{
+    let data = JSON.parse(jsonText);
+    console.log(data);
+}
+catch(err){
+    console.log("JSON parse error:",err.message);
+}
+
+try{
+    let num = 10;
+    num();
+}
+catch(err){
+    console.log("Caught error:",err.name);
+}
