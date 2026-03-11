dd0002363949a4abdf90de8aecb305331a6b1d84
dsjeevitha643-stack <dsjeevitha643@gmail.com>
2026-03-11 12:36:33 +0530

    saving and reading the session storage

---
+++ W4/D3/P3.js
@@ -0,0 +1,28 @@
+
+const themeInput = document.getElementById("themeInput");
+const output = document.getElementById("output");
+
+document.getElementById("saveBtn").addEventListener("click",function(){
+    sessionStorage.setItem("theme",themeInput.value);
+    sessionStorage.setItem("userName","Revathi");
+    sessionStorage.setItem("loggedIn","true");
+    console.log("Saved theme: ",themeInput.value);
+    output.textContent = "Successfully saved to session Storage";
+    output.style.color = "green";
+});
+
+document.getElementById("readBtn").addEventListener("click",function(){
+    const theme = sessionStorage.getItem("theme");
+    output.textContent = "theme is: "+ theme;
+    output.style.color = "green";
+});
+document.getElementById("removeBtn").addEventListener("click",function(){
+    sessionStorage.removeItem("loggedIn");
+    output.textContent = "Removed 'loggedIn' ";
+    output.style.color = "green";
+});
+document.getElementById("clearBtn").addEventListener("click",function(){
+    sessionStorage.clear();
+    output.textContent = "Deleted local Storage"
+    output.style.color = "red";
+});
