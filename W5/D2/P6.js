893c489f64ec5f50f6fea5da6d7a70b4f96df3cd
dsjeevitha643-stack <dsjeevitha643@gmail.com>
2026-03-17 13:30:12 +0530

    Callback nesting

---
+++ W5/D2/P6.js
@@ -0,0 +1,21 @@
+// Callback nesting 
+console.log("Starting nested callback flow");
+
+setTimeout(function () {
+    console.log("Step 1: user loaded");
+
+
+    setTimeout(function () {
+        console.log("Step 2: order loaded");
+
+
+        setTimeout(function () {
+            console.log("Step 3: payments loaded");
+
+            setTimeout(function () {
+                console.log("Step 4: Shipment loaded.");
+                console.log("We are in callback hell!!!");
+            }, 1000);
+        },800);
+    },600);
+},400);
